5434001ea0cea73356905a679a0d23090bce3573
Illia <jenerieshope@gmail.com>
2026-01-28 11:57:02 +0100

    test

--- app/output/output.py
+++ app/output/output.py
@@ -1,4 +1,47 @@
+from tkinter import END
+import pyodbc
+taskdb = 'somedb'
+class ConnectionManager:
+    def __init__(self):
+
+        try:
+            server = 'localhost'  # or '127.0.0.1'
+            port = '1433'
+            database = 'master'  # Default database
+            username = 'sa'
+            password = 'PASSword8'  # Your password
+            driver = '{ODBC Driver 18 for SQL Server}'  # Most common
+            
+            # Connection string
+            conn_str = f'DRIVER={driver};SERVER={server},{port};DATABASE={database};UID={username};PWD={password};Encrypt=no;TrustServerCertificate=yes'
+
+            self.congif = conn_str
+            self.connection = None
+            self.connect()
+        except:
+            print("Connection failed. The most common reason is a driver version. Check your ODBC Driver version. P.S. Should be ODBC Driver 18 for SQL Server.")
+        END
+
+        
+
+    def connect(self):
+        if not self.connection:
+            # You MUST call the function with your config string here
+            self.connection = pyodbc.connect(self.congif) 
+            self.connection.autocommit = True # - just for testing purposes. 
+
+        return self.connection
+    
+    def disconnect(self):
+        if self.connection:
+            self.connection.close()
+            self.connection = None
+        print("Connection closed.")
+        
 
 
-def sqlToXML():
-    print("SQL TO XML initiated.")
+
+
+
+
+    
